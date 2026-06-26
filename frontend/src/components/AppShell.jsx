8efe529791c9fbb29965d06008c1635d2585ee64
Esmael <esmael6360@gmail.com>
2026-06-26 09:53:33 +0300

    add app shell

--- frontend/src/components/AppShell.jsx
+++ frontend/src/components/AppShell.jsx
@@ -7,7 +7,7 @@ function AppShell({ children }) {
   const { lang, setLang, t, systemNameShort } = useLanguage();
   const location = useLocation();
   const isLogin = location.pathname === '/login';
-
+// comment
   if (isLogin) {
     return children;
   }
