35d943857613a1f77af7e3fe79bd91eacbcdc4db
Esmael <esmael6360@gmail.com>
2026-06-30 16:10:34 +0300

    update city logo

--- frontend/src/components/ThemeToggle.jsx
+++ frontend/src/components/ThemeToggle.jsx
@@ -1,6 +1,6 @@
 import { useLanguage } from '../context/LanguageContext';
 import { useTheme } from '../context/ThemeContext';
-// comm
+// commentss
 function ThemeToggle({ className = '' }) {
   const { isDark, toggleTheme } = useTheme();
   const { t } = useLanguage();
