ea0cef1dee8986cfe7e099b5f119bb64a8833fad
Esmael <esmael6360@gmail.com>
2026-06-30 12:34:12 +0300

    lk

--- frontend/src/components/AppShell.jsx
+++ frontend/src/components/AppShell.jsx
@@ -7,7 +7,7 @@ function AppShell({ children }) {
   const { lang, setLang, t, systemNameShort } = useLanguage();
   const location = useLocation();
   const isLogin = location.pathname === '/login';
-  // comm
+  // comment
   if (isLogin) {
     return children;
   }
