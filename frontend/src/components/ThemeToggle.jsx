0bfdf63b196dadad58216bfd11e37fce73a7101d
Esmael <esmael6360@gmail.com>
2026-07-01 10:49:10 +0300

    lk

--- frontend/src/components/ThemeToggle.jsx
+++ frontend/src/components/ThemeToggle.jsx
@@ -1,6 +1,6 @@
 import { useLanguage } from '../context/LanguageContext';
 import { useTheme } from '../context/ThemeContext';
-// comments
+// comment
 function ThemeToggle({ className = '' }) {
   const { isDark, toggleTheme } = useTheme();
   const { t } = useLanguage();
